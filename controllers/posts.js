c1a82a5a2a55c6f011d8639c5c7330079471f1dd
Shailyn Moore <m.shailyn@yahoo.com>
2022-09-18 11:30:47 -0700

    add to-do list on dashboard

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,7 +10,7 @@ module.exports = {
       const posts = await Post.find({ user: req.user.id });
       const todoItems = await Todo.find({userId:req.user.id})
       const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { posts: posts, user: req.user,todos: todoItems, left: itemsLeft });
     } catch (err) {
       console.log(err);
     }
