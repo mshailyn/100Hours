848d6d2e2aad2b45b48e9ffdf8683196834f613a
Shailyn Moore <m.shailyn@yahoo.com>
2022-10-27 14:30:55 -0700

    added dropdown menu on budget page

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -4,11 +4,20 @@ const moment = require('moment');
 module.exports = {
     getBudget: async (req,res)=>{
         try{
-            const thisBudget = await Budget.find({userId:req.user.id, date: req.params.date })
+            let thisBudget
+            let reqDate
+            if(!req.body.month){
+                thisBudget = await Budget.find({userId:req.user.id, date: req.params.date })
+                reqDate = req.params.date
+            }else{
+                thisBudget = await Budget.find({userId:req.user.id, date: req.body.month })
+                 reqDate = req.body.month
+
+            }
             let moneyLeft = thisBudget.length < 1 ? 0 :(thisBudget[0].monthBudget - (thisBudget[0].rent + thisBudget[0].utilities + thisBudget[0].car + thisBudget[0].gas + thisBudget[0].food + thisBudget[0].debt + thisBudget[0].subscription + thisBudget[0].savings) );
-            const reqDate = req.params.date
             console.log(req.params.date)
-            res.render("budget.ejs", {budget: thisBudget[thisBudget.length-1], user: req.user, left: moneyLeft, reqDate:req.params.date, moment: moment })
+            console.log(req.body.month)
+            res.render("budget.ejs", {budget: thisBudget[thisBudget.length-1], user: req.user, left: moneyLeft, reqDate: reqDate, moment: moment })
         }catch(err){
             console.log(err)
         }
